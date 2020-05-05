d8db6c4574d50068bc32e6c544d5953bb2662805
Marina00Matta <marinamedhat_19@hotmail.com>
2020-05-05 18:01:08 +0200

    structuring the folders and classes

---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Card, CardContent , Typography, Grid} from '@material-ui/core';
+
+import styles from './Cards.module.css'
+
+const Cards = (props) => {
+    console.log(props);
+
+    return(
+        <div className={styles.container}>
+            <Grid container spacing={3} justify="center">
+                <Grid>
+
+                </Grid>
+            </Grid>
+        </div>
+            )
+}
+
+export default Cards;
