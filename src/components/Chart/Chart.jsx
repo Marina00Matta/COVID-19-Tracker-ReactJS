7c4df98019047570b63fb3b897357a1b732cf773
Marina00Matta <marinamedhat_19@hotmail.com>
2020-05-06 17:51:15 +0200

    barchart and linechart per country

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -39,7 +39,29 @@ const Chart = ({data: { confirmed, deaths, recovered}, country}) => {
         />) : null
     );
     
-       
+        const barChart = (
+            confirmed
+            ? (
+                <Bar
+            data = {{
+                labels: ['Infected', 'Recovered', 'Deaths'],
+                datasets: [{
+                    label: 'People',
+                    backgroundColor: ['rgba(0,0,255,0.5)',
+                                     'rgba(0,255,0,0.5)',
+                                      'rgba(255,0,0,0.5)'],
+                    data: [confirmed.value, recovered.value, deaths.value]                
+                }]
+            }}  
+            
+            options = {{
+
+                legend: { display: false },
+                title: { display: true, text: `Current state in ${country}`},
+            }}
+                />
+            ) : null
+        );
     return(
         <div className={styles.container}>
             {country ? barChart : lineChart}
